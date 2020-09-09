6df993df69381b7cff67c7928d329af87c832568
hruj <hrushjaan@gmail.com>
2020-09-09 10:40:28 +0530

    Changes Integrated

--- front-back.py
+++ front-back.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect  
 from flask_mysqldb import MySQL 
+from werkzeug.utils import secure_filename
 import yaml 
 
 
@@ -10,7 +11,9 @@ app.config['MYSQLDB_HOST'] = db['mysql_host']
 app.config['MYSQLDB_USER'] = db['mysql_user']
 app.config['MYSQLDB_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
- 
+app.config['MYSQL_UPLOAD_FILES'] = db['mysql_fileStored']
+
+ALLOWED_EXTENSIONS = {'pdf', 'img'}
 mysql = MySQL(app)
 
 
@@ -38,15 +41,27 @@ def upload():
         for i in range(1,19):
             file_name = request.form['f{}'.format(i)]
             field_name = request.form['d{}'.format(i)]
-            cursor = mysql.connection.cursor()
-            cursor.execute("INSERT INTO file_storage (file_name, field_name) VALUES(%s, %s);",(file_name, field_name))
-            mysql.connection.commit()
-            cursor.close()
+        if 'file' not in request.files:
+            flash('Not allowed!')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected!')
+            return redirect(request.url)
+        if file and file_upload(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['MYSQL_UPLOAD_FILES'], filename))
+            return redirect(url_for('upload', filename=filename)) 
+
     return render_template('upload.html')
 
 @app.route('/my_redirect')
 def my_redirect():
     return redirect(url_for('upload',_anchor='upload_files'))
+
+def file_upload(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == "__main__":
     app.run(debug=True)
