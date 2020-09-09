eb4837f2b2bf610c00563adddab5cf2a1c5e6904
hruj <hrushjaan@gmail.com>
2020-09-09 10:10:12 +0530

    Changes Integrated

--- front-back.py
+++ front-back.py
@@ -31,13 +31,22 @@ def index():
 def home():
     return render_template('home.html')
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET','POST'])
 def upload():
+    if request.method =='POST':
+        upload_material = request.form
+        for i in range(1,19):
+            file_name = request.form['f{}'.format(i)]
+            field_name = request.form['d{}'.format(i)]
+            cursor = mysql.connection.cursor()
+            cursor.execute("INSERT INTO file_storage (file_name, field_name) VALUES(%s, %s);",(file_name, field_name))
+            mysql.connection.commit()
+            cursor.close()
     return render_template('upload.html')
 
 @app.route('/my_redirect')
 def my_redirect():
-    return redirect(url_for('upload',_anchor='my_anchor'))
+    return redirect(url_for('upload',_anchor='upload_files'))
 
 if __name__ == "__main__":
     app.run(debug=True)
